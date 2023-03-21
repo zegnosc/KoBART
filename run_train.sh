05fbb0f716b83e434835097f63868ef7c0f6de65
zegnosc <111365534+zegnosc@users.noreply.github.com>
2023-03-21 10:24:50 +0900

    배치 사이즈 수정

--- run_train.sh
+++ run_train.sh
@@ -2,5 +2,5 @@ python train.py --gradient_clip_val 1.0 \
                 --max_epochs 50 \
                 --default_root_dir logs \
                 --gpus 1 \
-                --batch_size 2 \
+                --batch_size 1 \
                 --num_workers 4
